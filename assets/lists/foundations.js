eb786dc06b2898bb68446a8767da113c4a210070
Edward Delmonico <ed.delmonico@gmail.com>
2025-02-09 21:13:39 +0200

    more lists

---
+++ assets/lists/foundations.js
@@ -0,0 +1,10 @@
+module.exports = [
+    'Established',
+    'Itinerant',
+    'Connected',
+    'Eremetic',
+    'Mendicant',
+    'Iconoclastic',
+    'Stalwart',
+    'Bizarre'
+]
---
+++ assets/lists/hearts.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        name: 'galant',
+        family: 'secrets',
+        animal: 'ravens',
+        object: 'book'
+    },
+    {
+        name: 'empath',
+        family: 'visions',
+        animal: 'swan',
+        object: 'blade'
+    },
+    {
+        name: 'stoic',
+        family: 'mysteries',
+        animal: 'rat',
+        object: 'mirror'
+    },
+    {
+        name: 'ardent',
+        family: 'notions',
+        animal: 'cat',
+        object: 'clock'
+    }
+]
